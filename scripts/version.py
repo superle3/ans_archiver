cfd82ea87526e3ef9d3961e1505d2e400c93980b
superle3 <987654321.yo.na.than@gmail.com>
2026-02-15 18:54:38 +0100

    feat: check if git tree is empty in version script

--- scripts/version.py
+++ scripts/version.py
@@ -1,10 +1,17 @@
-# make sure git tree is empty
 import os
+import subprocess
 import sys
 import tomllib
 
 
-...
+# make sure git tree is empty
+message = subprocess.run(
+    ["git", "status", "--porcelain"], capture_output=True, text=True
+).stdout.strip()
+if message != "":
+    raise ValueError(
+        "Git tree is not clean. Please commit or stash your changes before running this script."
+    )
 # upgrade version
 os.system(f"uv version --bump {sys.argv[1]}")
 
@@ -13,6 +20,6 @@ os.system("git add .")
 with open("pyproject.toml", "r", encoding="utf-8") as f:
     project = tomllib.loads(f.read())
     project_version = project["project"]["version"]
-os.system(f"git commit -m '{project_version}'")
+os.system(f"git commit -m {project_version}")
 os.system(f"git tag -a -s {project_version} -m {project_version}")
 os.system("git push --follow-tags")
